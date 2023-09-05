f74c0a49035b5d5571574c2d3dd3c2a8e850a08e
Juanbautissta <bauespindola14@gmail.com>
2023-09-04 21:13:18 -0300

    funcion results

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const questionController = require('./server/controllers/questionController');
 
 app.get('/api/questions', questionController.getAll);
 app.post('/api/answer', questionController.create);
+app.post('/api/results', questionController.results);
+
 
 app.listen(8000, () => {
     console.log('Servidor corriendo en puerto 8000');
